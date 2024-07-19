c71e17654762a1e8e0b26e8ed011ca7625bf8fa7
devilsoloanonymous <nsolo8677@gmail.com>
2024-07-19 15:56:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+nt.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission
 
     // Get the values from the form
